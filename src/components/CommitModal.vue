2d931dcfc67bb9bef78c6bf3bf0b1e884e416d58
Jan-Peter Voigt <jpv@beethovens-werkstatt.de>
2022-10-18 11:23:16 +0200

    indentation

--- src/components/CommitModal.vue
+++ src/components/CommitModal.vue
@@ -1,25 +1,25 @@
 <template>
 
   <div class="modal" :class="{ active }" id="commit-gh-modal">
-  <a href="#close" @click="closeModal()" class="modal-overlay" aria-label="Close"></a>
-  <div class="modal-container">
-    <div class="modal-header">
-      <a href="#close" @click="closeModal()" class="btn btn-clear float-right" aria-label="Close"></a>
-      <div class="modal-title h5">Commit {{ filepath }}</div>
-    </div>
-    <div class="modal-body">
-      <div class="content">
-        <textarea placeholder="commit message" class="commit-message" v-model="message"></textarea>
+    <a href="#close" @click="closeModal()" class="modal-overlay" aria-label="Close"></a>
+    <div class="modal-container">
+      <div class="modal-header">
+        <a href="#close" @click="closeModal()" class="btn btn-clear float-right" aria-label="Close"></a>
+        <div class="modal-title h5">Commit {{ filepath }}</div>
       </div>
-    </div>
-    <div class="modal-footer">
-      <div class="btn-group">
-        <button class="btn" @click="closeModal()">Cancel</button>
-        <button class="btn btn-primary" @click="main()">Commit</button>
+      <div class="modal-body">
+        <div class="content">
+          <textarea placeholder="commit message" class="commit-message" v-model="message"></textarea>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <div class="btn-group">
+          <button class="btn" @click="closeModal()">Cancel</button>
+          <button class="btn btn-primary" @click="main()">Commit</button>
+        </div>
       </div>
     </div>
   </div>
- </div>
 
 </template>
 
