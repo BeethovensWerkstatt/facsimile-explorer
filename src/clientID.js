dfd7f753d1ca902302715aa56c10fd2871829950
jpvoigt <jp.voigt@gmx.de>
2023-06-12 11:09:12 +0200

    load CLIENT_ID from config.json if not in env

--- src/clientID.js
+++ src/clientID.js
@@ -1,6 +1,6 @@
 // if VUE_APP_CLIENT_ID is given return that, otherwise try config.json
 const CLIENT_ID = new Promise((resolve, reject) => {
-  if (process.env.VUE_APP_CLIENT_ID) {
+  if (process.env.VUE_APP_CLIENT_ID && process.env.VUE_APP_CLIENT_ID !== '<CLIENT_ID>') {
     resolve(process.env.VUE_APP_CLIENT_ID)
   } else {
     const configURL = process.env.BASE_URL + 'config.json'
