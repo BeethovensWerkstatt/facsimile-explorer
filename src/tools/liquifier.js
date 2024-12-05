9b621a89f190dd35bf8d15013d36118215787ed6
Jan-Peter Voigt <jpv@schoene-noten.de>
2024-12-05 16:52:29 +0100

    try liquifier proxy

---
+++ src/tools/liquifier.js
@@ -0,0 +1,19 @@
+/*
+import * as _annotatedTranscripts from 'liquifier/src/annotatedTranscripts.js'
+import * as _diplomaticTranscripts from 'liquifier/src/diplomaticTranscripts.js'
+import * as _fluidTranscripts from 'liquifier/src/fluidTranscripts.js'
+import * as _liquiUtils from 'liquifier/src/utils.js'
+import * as _verovioHandler from 'liquifier/src/verovioHandler.js'
+
+export const annotatedTranscripts = _annotatedTranscripts
+export const diplomaticTranscripts = _diplomaticTranscripts
+export const fluidTranscripts = _fluidTranscripts
+export const liquiUtils = _liquiUtils
+export const verovioHandler = _verovioHandler
+
+export default { annotatedTranscripts, diplomaticTranscripts, fluidTranscripts, liquiUtils, verovioHandler }
+*/
+
+import { prepareDtForRendering } from 'liquifier/src/diplomaticTranscripts'
+
+export default { prepareDtForRendering }
