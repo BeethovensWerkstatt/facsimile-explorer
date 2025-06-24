f6a3558bfe57df5be2ca5bf03e79e669d31b151e
jpvoigt <jp.voigt@gmx.de>
2025-06-24 09:37:59 +0200

    init-dev.sh

--- init-dev.sh
+++ init-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # docker run --rm -ti -v $(pwd):/app -w /app node:latest npm i --legacy-peer-deps
-docker compose run -u 1000 webpack npm i --legacy-peer-deps
+docker compose run --rm -u 1000 webpack npm i --legacy-peer-deps
