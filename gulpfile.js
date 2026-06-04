4b5ae8f9db0679623dac010ea63ec158c1ec2138
jpvoigt <jp.voigt@gmx.de>
2026-06-04 10:03:29 +0200

    add thulemeier version

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 const gulp = require('gulp')
 const git = require('gulp-git')
 const fs = require('fs')
+const { execSync } = require('child_process')
+
+const readGitInfo = args => {
+  const stdout = execSync(`git ${args}`, { encoding: 'utf8' })
+  const v = stdout.split('\n')
+
+  return {
+    version: stdout,
+    date: v[0],
+    author: v[1],
+    subject: v[2],
+    commit: v[3]
+  }
+}
 
 const gitlog = file => async function () {
   const json = {}
@@ -26,6 +40,12 @@ const gitlog = file => async function () {
         function (err, stdout) {
           if (err) throw err
           json.branch = stdout.trim()
+          try {
+            json.thulemeier = readGitInfo('-C ../thulemeier log -n 1 --pretty="%ai%n%an%n%s%n%H%n"')
+            json.thulemeier.branch = execSync('git -C ../thulemeier rev-parse --abbrev-ref HEAD', { encoding: 'utf8' }).trim()
+          } catch (thulemeierErr) {
+            json.thulemeier = null
+          }
           fs.writeFile(file, JSON.stringify(json, null, 2), function () {
             console.log(json, 'fertig')
           })
