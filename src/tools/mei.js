cb079f6421bd145fd1e35d724e4833df56f9b8e9
Jan-Peter Voigt <jpv@schoene-noten.de>
2026-06-11 13:55:40 +0200

    where do the dot@corresp come from?

--- src/tools/mei.js
+++ src/tools/mei.js
@@ -143,14 +143,15 @@ export function generateDiplomaticElement (annotElem, shapes, bbox, svgPath, cor
     const dotElems = annotElem.querySelectorAll('dot')
     let dotidx = Array.from(dotElems).findIndex(dot => !dot.hasAttribute('corresp')) // find first dot element without corresp
     if (dotidx === -1) {
-      console.warn('no dot element without corresp found, this should not happen!', dotElems)
+      console.warn(6883, 'no dot element without corresp found, this should not happen!', dotElems)
       dotidx = dotElems.length - 1 // fallback to first last element
     }
     const dotElem = dotElems[dotidx]
     dotElem.setAttribute('corresp', corresp)
-    console.log(6883, 'setting corresp on dot element', dotElem)
     const refs = dotElem.getAttribute('corresp') ? dotElem.getAttribute('corresp').split(' ') : []
+    console.log(6883, 'setting corresp on dot element', dotElem, refs)
     refs.push(newCorresp)
+    console.log(6883, 'setting corresp on dot element', dotElem, refs)
     dotElem.setAttribute('corresp', refs.join(' '))
   } else {
     annotElem.setAttribute('corresp', corresp)
